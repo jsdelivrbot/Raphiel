2789131bde8b2d8d379da9ec242dd6437dbade20
FreezePhoenix <683264@sylvaniastudents.org>
2018-05-07 17:48:01 -0400

    Update run-headless.js

--- run-headless.js
+++ run-headless.js
@@ -3,15 +3,7 @@ var fs = require('fs');
 var repl = require('readline');
 var util = require('util');
 
-var config = {
-	"email": "you can guess",
-	"password": "what these are",
-
-	"siteUrl": "https://stackexchange.com",
-	"roomUrl": "https://chat.stackexchange.com/rooms/75397"
-}
-config.email = localStorage.email
-config.password = localStorage.password
+var config = require('run-headless.config.json')
 
 cri(async (client) => {
     try {
